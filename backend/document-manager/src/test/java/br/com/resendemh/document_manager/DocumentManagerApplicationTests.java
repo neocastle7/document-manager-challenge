6eb15684745cea501d887893147ba434f83614e6
neocastle7 <neocastle7@gmail.com>
2026-03-13 13:16:48 -0300

    Melhorias de Código

--- backend/document-manager/src/test/java/br/com/resendemh/document_manager/DocumentManagerApplicationTests.java
+++ backend/document-manager/src/test/java/br/com/resendemh/document_manager/DocumentManagerApplicationTests.java
@@ -1,13 +1,20 @@
 package br.com.resendemh.document_manager;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class DocumentManagerApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(context, "O contexto da aplicação não deve ser nulo");
 	}
 
 }
